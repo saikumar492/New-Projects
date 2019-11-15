3f1c5fa7e7c2016ae0b66c9c05e9716d69e7e1a9
saikumar492 <saikumar4559.sk@gmail.com>
2019-11-15 15:54:28 +0530

    fibonacci

--- Example.py
+++ Example.py
@@ -1,20 +1,20 @@
-def fibonacci(n): 
-    a = 0
-    b = 1
-    if n < 0: 
-        print("Incorrect input") 
-    elif n == 0: 
-        return a 
-    elif n == 1: 
-        return b 
-    else: 
-        for i in range(2,n): 
-            c = a + b 
-            a = b 
-            b = c 
-        return b 
-  
-# Driver Program 
-  
-print(fibonacci(9)) 
-  
+# Program to display the Fibonacci sequence up to n-th term
+nterms = int(input("How many terms? "))
+# first two terms
+n1, n2 = 0, 1
+count = 0
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1, end = " ")
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
